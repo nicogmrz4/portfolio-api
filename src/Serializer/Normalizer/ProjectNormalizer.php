384591125a5d685c6fdfbfa5f766d782c36fc389
Nicolas <nicolasgalarza40@gmail.com>
2024-02-24 14:37:48 -0300

    Normalization for resolve images path

---
+++ src/Serializer/Normalizer/ProjectNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\Project;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Vich\UploaderBundle\Storage\StorageInterface;
+
+class ProjectNormalizer implements NormalizerInterface
+{
+    private const ALREADY_CALLED = 'PROJECT_NORMALIZER_ALREADY_CALLED';
+
+    public function __construct(
+        private StorageInterface $storage,
+        #[Autowire(service: 'serializer.normalizer.object')]
+        private readonly NormalizerInterface $normalizer,
+    ) {}
+
+    public function normalize($object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    {
+        $context[self::ALREADY_CALLED] = true;
+
+        $object->setPreview(
+            $this->storage->resolveUri($object, 'file')
+        );
+
+        
+        return $this->normalizer->normalize($object, $format, $context);
+    }
+
+    public function supportsNormalization($data, ?string $format = null, array $context = []): bool
+    {
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
+        return $data instanceof Project;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            Project::class => true,
+        ];
+    }
+}
---
+++ src/Serializer/Normalizer/SkillNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\Skill;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Vich\UploaderBundle\Storage\StorageInterface;
+
+class SkillNormalizer implements NormalizerInterface
+{
+    private const ALREADY_CALLED = 'SKILL_NORMALIZER_ALREADY_CALLED';
+
+    public function __construct(
+        private StorageInterface $storage,
+        #[Autowire(service: 'serializer.normalizer.object')]
+        private readonly NormalizerInterface $normalizer,
+    ) {}
+
+    public function normalize($object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    {
+        $context[self::ALREADY_CALLED] = true;
+
+        $object->setIcon(
+            $this->storage->resolveUri($object, 'file')
+        );
+
+        
+        return $this->normalizer->normalize($object, $format, $context);
+    }
+
+    public function supportsNormalization($data, ?string $format = null, array $context = []): bool
+    {
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
+        return $data instanceof Skill;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            Skill::class => true,
+        ];
+    }
+}
