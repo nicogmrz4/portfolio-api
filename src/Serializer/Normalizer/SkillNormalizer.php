2d95fe3e9326a28897d17b398745409b4a41e43f
Nicolas <nicolasgalarza40@gmail.com>
2024-02-28 11:34:44 -0300

    Fix skill normalization

--- src/Serializer/Normalizer/SkillNormalizer.php
+++ src/Serializer/Normalizer/SkillNormalizer.php
@@ -13,7 +13,7 @@ class SkillNormalizer implements NormalizerInterface
 
     public function __construct(
         private StorageInterface $storage,
-        #[Autowire(service: 'serializer.normalizer.object')]
+        #[Autowire(service: 'api_platform.jsonld.normalizer.item')]
         private readonly NormalizerInterface $normalizer,
     ) {}
 
